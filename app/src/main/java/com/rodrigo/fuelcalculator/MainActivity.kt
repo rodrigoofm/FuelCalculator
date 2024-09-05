181d95b905262c96c4bff056b05b0b8b954ba10b
Rodrigo Fernandes <f.rodrigo095@gmail.com>
2024-09-05 07:09:48 -0300

    create layout

--- app/src/main/java/com/rodrigo/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/rodrigo/fuelcalculator/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.rodrigo.fuelcalculator
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rodrigo.fuelcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private val binding by lazy {
+    ActivityMainBinding.inflate(layoutInflater)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContentView(R.layout.activity_main)
+    setContentView(binding.root)
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
+    }
+    setupListeners()
+  }
+
+  private fun setupListeners() {
+    binding.buttonNext.setOnClickListener {
+      startActivity(Intent(this, CalculatorActivity::class.java))
     }
   }
 }
